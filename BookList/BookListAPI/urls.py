1d923c9a949479ff38ffb616b83b04a179e0d085
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-11 21:14:13 -0700

    Routing class-based views

--- BookList/BookListAPI/urls.py
+++ BookList/BookListAPI/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+
+# URL pattern for a class method
+# urlpatterns = [
+#     path('orders', views.Orders.listOrders)
+# ]
+
+# URL pattern for routing a class-based view
 urlpatterns = [
-    path('orders', views.Orders.listOrders)
+    path('books/<int:pk>', views.BookView.as_view())
 ]
